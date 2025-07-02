28895ca59d5cd46200cedf80fc57fb594093925f
BeeFeng <108512312+Microspf@users.noreply.github.com>
2025-07-02 14:30:51 +0800

    Update mycpu_tb.v

--- sim/mycpu_tb.v
+++ sim/mycpu_tb.v
@@ -90,22 +90,22 @@ begin
 
     #100000
     touch_btn = 4'b0001;
-    #50
+    #100
     touch_btn = 4'b0000;
 
     #100000
     touch_btn = 4'b0010;
-    #50
+    #100
     touch_btn = 4'b0000;
 
     #100000
     touch_btn = 4'b0100;
-    #50
+    #100
     touch_btn = 4'b0000;
 
     #100000
     touch_btn = 4'b1000;
-    #50
+    #100
     touch_btn = 4'b0000;
 
 end
